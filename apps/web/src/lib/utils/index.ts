4dee7e5f6f95a284b5bbb9f848e107ce4139c7b3
Vladislav <vlad.kznetsov@gmail.com>
2026-03-10 13:51:13 +0300

    refactor(web): add DEBOUNCE config constants

--- apps/web/src/lib/utils/index.ts
+++ apps/web/src/lib/utils/index.ts
@@ -1,6 +1,7 @@
 export { logger } from './logger';
 export { ROUTES } from './routes';
 export { debounce } from './debounce';
+export { DEBOUNCE } from './config';
 export {
 	createValidator,
 	createFormFieldValidator,
