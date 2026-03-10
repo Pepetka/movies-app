edd35147760bbda602f367d6270d672f1a064eb1
Vladislav <vlad.kznetsov@gmail.com>
2026-03-10 13:27:39 +0300

    refactor(web): add destroy method to query, remove enabled option

--- apps/web/src/lib/query/types.ts
+++ apps/web/src/lib/query/types.ts
@@ -8,19 +8,20 @@ export interface QueryOptions<T> {
 	key: unknown[];
 	fetcher: (signal: AbortSignal) => Promise<T>;
 	tags?: string[];
-	enabled?: boolean;
 	debug?: boolean;
 }
 
 export interface QueryActions {
 	refetch: () => Promise<void>;
 	reset: () => void;
+	destroy: () => void;
 }
 
 export interface QueryResult<T> extends QueryState<T> {
 	readonly isError: boolean;
 	refetch: () => Promise<void>;
 	reset: () => void;
+	destroy: () => void;
 }
 
 export interface RegistryEntry {
