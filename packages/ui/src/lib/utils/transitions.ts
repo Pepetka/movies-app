fc771c0056dedff7240b84ca3143aaacdd7f5f97
Vladislav <vlad.kznetsov@gmail.com>
2026-04-12 00:02:32 +0300

    refactor(ui): extract transition duration constant

--- packages/ui/src/lib/utils/transitions.ts
+++ packages/ui/src/lib/utils/transitions.ts
@@ -1,14 +1,16 @@
-export const OVERLAY_FADE = { duration: 200 };
+const DURATION = 200;
 
-export const MODAL_FLY = { y: 20, duration: 200 };
+export const OVERLAY_FADE = { duration: DURATION };
+
+export const MODAL_FLY = { y: 20, duration: DURATION };
 
 export const DRAWER_FLY = {
-	left: (size: string) => ({ x: `-${size || '320px'}`, duration: 200 }),
-	right: (size: string) => ({ x: size || '320px', duration: 200 }),
-	bottom: { y: '100%', duration: 200 }
+	left: (size: string) => ({ x: `-${size || '320px'}`, duration: DURATION }),
+	right: (size: string) => ({ x: size || '320px', duration: DURATION }),
+	bottom: { y: '100%', duration: DURATION }
 };
 
 export const DROPDOWN_FLY = {
-	top: { y: 20, duration: 200 },
-	bottom: { y: -20, duration: 200 }
+	top: { y: 20, duration: DURATION },
+	bottom: { y: -20, duration: DURATION }
 };
